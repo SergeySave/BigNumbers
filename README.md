1cc53d0950aee4819a09c63bd21a05db81d1dc24
Sergey Savelyev <sergeysav.nn@gmail.com>
2018-04-24 18:23:02 -0400

    Times the Integers

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Big Numbers
+
+## 2s complement integers
+
+### Addition
+
+Unit | Long | Int128 | Int256 | Int512 | Int1024 | Int2048
+-----|------|--------|--------|--------|---------|---------
+ ms  | 2.435×10<sup>-8</sup> | 1.126×10<sup>-6</sup> | 3.224×10<sup>-6</sup> | 5.477×10<sup>-6</sup> | 1.094×10<sup>-5</sup> | 2.097×10<sup>-5</sup>
+Long Multiples | 1 | 46.276 | 127.285 | 218.736 | 457.795 | 849.149
---
+++ src/Timing.java
@@ -0,0 +1,36 @@
+/**
+ * @author sergeys
+ */
+public class Timing {
+
+    private static final Int2048 added = Int2048.from(13);
+    private static final int     TIMES = 1_000_000_000;
+
+    public static void main(String[] args) {
+        Int2048 num = new Int2048(Int2048.ONE);
+        long time128 = 0;
+        {
+            long before = System.nanoTime();
+            for (int i = 0; i < TIMES; i++) {
+                num.add(added);
+                //                num.add(Int2048.ONE);
+            }
+            long after = System.nanoTime();
+            time128 += (after - before);
+        }
+        long n = 1;
+        long timelong = 0;
+        {
+            long before = System.nanoTime();
+            for (int i = 0; i < TIMES; i++) {
+                n += 13;
+                //                n += 1;
+            }
+            long after = System.nanoTime();
+            timelong += (after - before);
+        }
+        System.out.println("Int:  " + ((time128 / (double) TIMES) * 1e-6));
+        System.out.println("Long: " + ((timelong / (double) TIMES) * 1e-6));
+        System.out.println("Div:  " + ((time128 / (double) timelong)));
+    }
+}
